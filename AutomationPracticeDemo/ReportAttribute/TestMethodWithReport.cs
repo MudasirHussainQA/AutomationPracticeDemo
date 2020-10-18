2d7a1fdcd9998b4a58fc89997fa8546ba75c0d4f
Mudasir Hussain <mudsrhussain@yahoo.com>
2020-10-18 18:56:55 +0530

    Added Reporter and Extent Report

---
+++ AutomationPracticeDemo/ReportAttribute/TestMethodWithReport.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationPracticeDemo.ReportAttribute
+{
+    //[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class TestMethodWithReport : TestMethodAttribute
+    {
+        private readonly object syslock = new object();
+        public override TestResult[] Execute(ITestMethod testMethod)
+        {
+            var name = testMethod.TestClassName + "." + testMethod.TestMethodName;
+            var result = base.Execute(testMethod);
+            var execution = result.FirstOrDefault();
+            var status = execution.Outcome;
+            var errormsg = execution?.TestFailureException?.Message;
+            var trace = execution?.TestFailureException?.StackTrace;
+
+            lock (syslock)
+            {
+                //object CustomExtentReporter = null;
+                CustomExtentReporter.GetInstance().AddToReport(name, "", status, errormsg + "\n" + trace);
+            }
+
+            return result;
+        }
+    }
+
+}
---
+++ AutomationPracticeDemo/Reporter/CustomExtentReporter.cs
@@ -0,0 +1,61 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationPracticeDemo.Reporter
+{
+    public class CustomExtentReporter
+    {
+        private readonly ExtentHtmlReporter extentHtmlReporter;
+
+        private readonly ExtentReports extentReports;
+
+        private static CustomExtentReporter customExtentReporter;
+
+        private CustomExtentReporter()
+        {
+            extentHtmlReporter = new ExtentHtmlReporter(@"C:\Users\Mudasir\Documents\Visual Studio 2015\Projects\AutomationPracticeDemo\TestDisplayResults");
+            extentReports = new ExtentReports();
+            extentReports.AttachReporter(extentHtmlReporter);
+        }
+
+        public static CustomExtentReporter GetInstance()
+        {
+            if (customExtentReporter == null)
+            {
+                customExtentReporter = new CustomExtentReporter();
+            }
+
+            return customExtentReporter;
+
+        }
+
+        public void AddToReport(string name, string description, UnitTestOutcome status, string error)
+        {
+            switch (status)
+            {
+                case UnitTestOutcome.Passed:
+                    extentReports.CreateTest(name, description).Pass("");
+                    break;
+                case UnitTestOutcome.Failed:
+                    extentReports.CreateTest(name, description).Fail(error);
+                    break;
+                default:
+                    extentReports.CreateTest(name, description).Skip("");
+                    break;
+            }
+        }
+
+        public void WriteReport()
+        {
+            extentReports?.Flush();
+        }
+
+
+    }
+}
